584b701a47074ae029d98430220bdcd174ec6aba
Aleksandar Ćirković <116209910+cireli@users.noreply.github.com>
2024-02-12 22:05:02 +0100

    uradjen login i njegov dizajn

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,63 +3,42 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login i Registracija</title>
-  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Prijava | Registracija</title>
+  <link href="/css/login.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+   
 </head>
+@include('navbar')
 <body>
 
-@include('navbar')
-
-<div class="container mt-5">
-  <div class="row justify-content-center">
-    <div class="col-md-6">
-      <div class="card">
-        <div class="card-body">
-          <h2 class="card-title">Login</h2>
+<div class="container" id="container">
+ 
+        <div class="form-container sign-in">
           <form method = "POST" action="/login">
             @csrf
-            <div class="form-group">
-              <input type="text" class="form-control" id="email" placeholder="Unesite email " name ="email" required>
-            </div>
-            <div class="form-group">
-              <input type="password" class="form-control" id="password" placeholder="Unesite lozinku" name="password" required>
-            </div>
-            <button type="submit" class="btn btn-secondary">Prijavite se</button>
+            <h1 class="h1-text">Prijava </h1 class="h1-text">
+            <br/>
+              <input type="text" id="email" placeholder="Unesite email " name ="email" required>
+              <input type="password" id="password" placeholder="Unesite lozinku" name="password" required>
+              <button type="submit">Prijavite se</button>
           </form>
         </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="container mt-5">
-  <div class="row justify-content-center">
-    <div class="col-md-6">
-      <div class="card">
-        <div class="card-body">
-          <h2 class="card-title">Registracija</h2>
-          <form method="POST" action="/register">
-            @csrf
-            <div class="form-group">
-              <input type="text" class="form-control" id="fullname" name= "fullname" placeholder="Unesite ime i prezime" required>
-            </div>
-            <div class="form-group">
-              <input type="email" class="form-control" id="reg_email" name="reg_email" placeholder="Unesite email adresu" email required>
-            </div>
-            <div class="form-group">
-              <input type="password" class="form-control" id="reg_password" name="reg_password" placeholder="Unesite lozinku" required>
-            </div>
-            <button type="submit" class="btn btn-secondary">Registrujte se</button>
-          </form>
+          <div class="toggle-container">
+        <div class="toggle">
+          <div class="toggle-panel toggle-right">
+            <h1 class="h1-text">Dobrodošao nazad!</h1 class="h1-text">
+            <p>
+              Unesi svoje podatke, kako bi mogao da koristiš sve mogućnosti
+              našeg sajta
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-</div>
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
+    </div> 
+      <script src="/js/script.js"></script>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
