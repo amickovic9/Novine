8976db5b0b3336d5b07d4e2044eca7123165f31b
Jelena <jelenadjalovicc@gmail.com>
2024-02-14 21:05:50 +0100

    USerSeeder za sve role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,29 +8,32 @@ use Illuminate\Database\Seeder;
 class UserSeeder extends Seeder
 {
     public function run()
-    {
-        $user = User::where('email', 'test@test.com')->first();
+{
+    $users = [
+        [
+            'name' => 'Admin Admin',
+            'email' => 'admin@test.com',
+            'password' => '$2a$12$kjODAm4GIq1KULYPtR5uxuoNxcH66PUtE.qfH4mPvRXebbFOry.SG',
+            'role' => 4,
+        ],
+        [
+            'name' => 'Editor Editor',
+            'email' => 'editor@test.com',
+            'password' => '$2a$12$kjODAm4GIq1KULYPtR5uxuoNxcH66PUtE.qfH4mPvRXebbFOry.SG',
+            'role' => 3,
+        ],
+        [
+            'name' => 'Journalist Journalist',
+            'email' => 'journalist@test.com',
+            'password' => '$2a$12$kjODAm4GIq1KULYPtR5uxuoNxcH66PUtE.qfH4mPvRXebbFOry.SG',
+            'role' => 2,
+        ],
+    ];
 
-        if (!$user) {
-            User::factory()->create([
-                'name' => 'Test Name1',
-                'email' => 'test@test.com',
-                'password' => '$2a$12$kjODAm4GIq1KULYPtR5uxuoNxcH66PUtE.qfH4mPvRXebbFOry.SG', 
-                'role' => 4,
-            ]);
+    foreach ($users as $user) {
+        if (!User::where('email', $user['email'])->first()) {
+            User::factory()->create($user);
         }
     }
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
-    }
+}
 }
