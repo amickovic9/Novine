8467c28e6f825dc9e1bcbb663c41992c893b38f1
Cope01 <petarurosevic4@gmail.com>
2024-02-14 19:34:32 +0100

    Izbrisani nepotrebni komentari iz News i User Factory-a

--- database/factories/NewsFactory.php
+++ database/factories/NewsFactory.php
@@ -8,18 +8,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class NewsFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = News::class;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         return [
@@ -31,5 +20,6 @@ class NewsFactory extends Factory
             'draft'=>0,
         ];
     }
+    
     
 }
